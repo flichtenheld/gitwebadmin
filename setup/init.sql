d40bb94a986eb3ea093e45428dd4ef2c1c3f779e
Frank Lichtenheld <flichtenheld@astaro.com>
2009-06-30 12:11:21 +0200

    Create logs_push table for use in post-receive hooks

--- setup/init.sql
+++ setup/init.sql
@@ -5,11 +5,14 @@ DROP TABLE writable CASCADE;
 DROP TABLE readable CASCADE;
 DROP TABLE members  CASCADE;
 DROP TABLE subscriptions CASCADE;
+DROP TABLE logs_push CASCADE;
 DROP ROLE gwa_webaccess;
+DROP ROLE gwa_gitaccess;
 
 BEGIN;
 
 CREATE ROLE gwa_webaccess LOGIN PASSWORD 'foobar';
+CREATE ROLE gwa_gitaccess;
 
 CREATE TABLE users (
        uid   TEXT PRIMARY KEY,
@@ -20,6 +23,7 @@ CREATE TABLE users (
 );
 CREATE INDEX users_name_idx ON users (name);
 GRANT SELECT, UPDATE ON users TO gwa_webaccess;
+GRANT SELECT ON users TO gwa_gitaccess;
 
 INSERT INTO users VALUES
        ('gitadm', 'Git Administrator');
@@ -58,6 +62,7 @@ CREATE TABLE repos (
 CREATE INDEX repos_name_idx ON repos (name);
 GRANT ALL ON repos TO gwa_webaccess;
 GRANT ALL ON SEQUENCE repos_id_seq TO gwa_webaccess;
+GRANT SELECT ON repos TO gwa_gitaccess;
 
 CREATE OR REPLACE FUNCTION repo_id (text) RETURNS int AS $$
        SELECT id FROM repos r WHERE r.name = $1
@@ -90,5 +95,19 @@ CREATE TABLE subscriptions (
        UNIQUE (rid, uid)
 );
 GRANT ALL ON subscriptions TO gwa_webaccess;
+GRANT SELECT ON subscriptions TO gwa_gitaccess;
+
+CREATE TABLE logs_push (
+       rid   INT NOT NULL REFERENCES repos(id) ON DELETE RESTRICT,
+       uid   TEXT NOT NULL REFERENCES users(uid) ON DELETE RESTRICT,
+       date  TIMESTAMP NOT NULL DEFAULT now(),
+       old_id TEXT NOT NULL CHECK (char_length(old_id) = 40),
+       new_id TEXT NOT NULL CHECK (char_length(new_id) = 40),
+       ref   TEXT NOT NULL,
+       notified BOOLEAN NOT NULL DEFAULT FALSE
+);
+GRANT SELECT, INSERT ON logs_push TO gwa_gitaccess;
+CREATE INDEX logs_push_uid_idx ON logs_push (uid);
+CREATE INDEX logs_push_ref_idx ON logs_push (ref);
 
 COMMIT;
