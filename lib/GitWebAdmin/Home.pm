df1081cf6aea2b406799b7629af942ce4464b40e
Frank Lichtenheld <flichtenheld@astaro.com>
2009-07-03 16:29:12 +0200

    Make the start page more informative

--- lib/GitWebAdmin/Home.pm
+++ lib/GitWebAdmin/Home.pm
@@ -10,8 +10,12 @@ sub start {
   my $db = $c->param('db');
   my $repos = $db->resultset('Repos')->search(
     { private => 0 }, { order_by => $c->query->param('sort_by') || 'name' });
+  my $groups = $db->resultset('Groups');
+  my $users = $db->resultset('Users');
 
-  return $c->tt_process({ repos => $repos });
+  return $c->tt_process({ repos => $repos,
+                          groups => $groups,
+                          users => $users });
 }
 
 1;
