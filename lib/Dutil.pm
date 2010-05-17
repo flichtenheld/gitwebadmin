6883d775c71d2f310f7e46a8bf92e25d6bdfdbf6
Frank Lichtenheld <flichtenheld@astaro.com>
2010-05-17 22:30:31 +0200

    [13747] Change license statements to GPL v2 or later
    While at it, update company name from Astaro AG to
    Astaro GmbH & Co. KG.

--- lib/Dutil.pm
+++ lib/Dutil.pm
@@ -1,5 +1,19 @@
 #
-#  Copyright (C) 2009 Astaro AG  www.astaro.com
+#  Copyright (C) 2009 Astaro GmbH & Co. KG  www.astaro.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 #  Author: Kai Baesler <kbaesler@astaro.com> 14.04.2009
 #
@@ -201,6 +215,16 @@ Kai Bäsler, E<lt>kbaesler@astaro.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009 Astaro AG  www.astaro.com
+Copyright (C) 2009 Astaro GmbH & Co. KG  www.astaro.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
 =cut
